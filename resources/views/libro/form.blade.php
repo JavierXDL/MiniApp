6227d26240be05c5a4df2e491850b965925de5cd
JavierXDL <yairjavier309@gmail.com>
2021-03-14 18:01:25 -0500

    SubiendoMiAPP

---
+++ resources/views/libro/form.blade.php
@@ -0,0 +1,55 @@
+<h1> {{ $modo }} Libro </h1>
+
+@if(count($errors)>0)
+
+    <div class="alert alert-danger" role="alert">
+    <ul>
+        @foreach($errors->all() as $error)
+        <li>    {{  $error }}   </li>
+    @endforeach
+    </ul>
+    </div>
+   
+@endif
+
+<!--Metodo del formulario mas vistoso-->
+<div class="form-group">
+<label for="Nombre"> Nombre </label>
+<input type="text"  class="form-control" name="Nombre" value=" {{ isset($pasar->Nombre)?$pasar->Nombre:old('Nombre') }}" id="Nombre">
+
+</div>
+<!--Fin del formulario mas vistoso-->
+
+
+<!--Metodo del formulario mas vistoso-->
+<div class="form-group">
+<label for="Autor"> Autor </label>
+<input type="text"  class="form-control" name="Autor" value=" {{ isset($pasar->Autor)?$pasar->Autor:old('Autor') }}"    id="Autor">
+
+</div>
+<!--Fin del formulario mas vistoso-->
+
+<!--Metodo del formulario mas vistoso-->
+<div class="form-group">
+
+<label for="DescripcionC"> Descripcion </label>
+<input type="text" class="form-control" name="DescripcionC" value=" {{ isset($pasar->DescripcionC)?$pasar->DescripcionC:old('DescripcionC') }}"  id="Descripcion">
+
+</div>
+<!--Fin del formulario mas vistoso-->
+
+
+<!--Metodo del formulario mas vistoso-->
+<div class="form-group">
+<label for="Portada"> Portada </label>
+
+@if(isset($pasar->Portada))
+<img src=" {{ asset('storage').'/'.$pasar->Portada }} "  widt="100" height="100" alt="">
+@endif
+<input type="file" class="form-control" name="Portada" value=""  id="Portada">
+
+</div>
+<!--Fin del formulario mas vistoso-->
+<input class="btn btn-success" type="submit" value=" {{ $modo }} datos"> 
+
+<a class="btn btn-primary" href="  {{ url('libro/') }} " > Regresar </a>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
+use Illuminate\Routing\RouteGroup;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+/*Route::get('/libro', function () {
+    return view('libro.index');
+}); 
+
+Route::get('/libro/create',[MenuController::class,'create']); */
+
+Route::resource('libro',MenuController::class)->middleware('auth');
+Auth::routes(['reset'=>false]);
+
+Route::get('/home', [MenuController::class, 'index'])->name('home');
+
+Route::middleware(['middleware' => 'auth'],function () {
+    Route::get('/', [MenuController::class, 'index'])->name('home');
+    
+});
+    
